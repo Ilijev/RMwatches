194165e3c68dbefcc3387179d7a49dc9a81aa56a
Bogdan Ilijev <bogdanilijev@yahoo.com>
2023-08-08 10:49:50 +0200

    legal terms error changed

--- src/pages/privacy and cookies policy/Imprint.tsx
+++ src/pages/privacy and cookies policy/Imprint.tsx
@@ -7,8 +7,8 @@ export default function Imprint({}: Props) {
     <div className="container py-5 fw-bold">
       <h2 className="mb-5">Impressum:</h2>
       <p className="">RM Watches Munich GmbH</p>
-      <p>Feringastr. 68</p>
-      <p>5774 Unterföhring</p>
+      <p>Feringastr. 6</p>
+      <p>85774 Unterföhring</p>
       <p>Deutschland</p>
       <p>Tel.: 0173 8971063</p>
       <p>E-Mail: info@rmwatches.de</p>
